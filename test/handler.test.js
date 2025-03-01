ab30be8e11b88c90d2bd1f836c8865d9cb9de877
chathurangaA <creedsadun94@gmail.com>
2025-03-01 18:19:23 +0530

    fix test error

--- test/handler.test.js
+++ test/handler.test.js
@@ -3,5 +3,5 @@ const { handler } = require('../src/handler');
 test('Lambda function returns correct response', async () => {
     const response = await handler({});
     expect(response.statusCode).toBe(200);
-    expect(JSON.parse(response.body).message).toBe("Hello from Lambda Code test! test fail");
+    expect(JSON.parse(response.body).message).toBe("Hello from Lambda Code test!");
 });
